a553c7822e51c05a9fe4be580d471cb389a8ecf6
akhilpr404 <akhilpr404@gmail.com>
2022-03-14 23:45:08 +0530

    commit on 14/03/2022 added axios and constants file for viewing dynamically the banner. but  there is an error occures module not found ./constants -- exports in baseUrl..time up..

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React ,{useEffect,useState} from 'react'
+import {API_KEY} from '../../Constants/constants'
 import "./Banner.css"
+import axios from "../../axios"
 function Banner() {
+  const [movie ,setMovie ] = useState()
+  useEffect(() => {
+      axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+        setMovie(response.data.results[0])
+      })
+      
+  }, [])
+  
   return (
     <div className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'>{movie.title}</h1>
             <div className='banner_buttons'>
                 <button className='button'>Play</button>
                 <button className='button'>My List</button> 
